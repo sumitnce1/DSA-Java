c887c384595e0508dc06aef5c09ee0a68305f90f
sumitnce1 <skumaronline1@gmail.com>
2023-07-18 15:32:02 +0530

    enqueueLast Day-2 Added

--- Day_2/src/com/dsajava/sumit/QueueArray.java
+++ Day_2/src/com/dsajava/sumit/QueueArray.java
@@ -1,39 +1,57 @@
 package com.dsajava.sumit;
 
+import java.nio.channels.IllegalSelectorException;
+
 public class QueueArray<T> {
-    private Object[] queue;
-    private int firstIndex;
-    private int lastIndex;
-    private int size;    
-    
-    public QueueArray (int capacity) {
-    	queue = new Object[capacity];
-    	firstIndex = -1;
-    	lastIndex = -1;
-    	size = 0;    	
-    }
-    
-    private int size() {
-    	return size;    	
-    }
-    private boolean isEmpty() {
-    	return size==0;
-    }
-    private boolean isFull() {
-    	return size== queue.length;
-    }
-    public void enqueueFirst(T element) {
-    	if(isFull()) {
-    		throw new IllegalStateException("Queue is Full.");
-    	}
-    	if(isEmpty()) {
-    		firstIndex = 0;
-    		lastIndex = 0;
-    	}
-    	else {
-    		firstIndex = (firstIndex-1 + queue.length) % queue.length;
-    	}
-    	queue[firstIndex] = element;
-    	size++;
-    }
+	private Object[] queue;
+	private int firstIndex;
+	private int lastIndex;
+	private int size;    
+
+	public QueueArray (int capacity) {
+		queue = new Object[capacity];
+		firstIndex = -1;
+		lastIndex = -1;
+		size = 0;    	
+	}
+
+	private int size() {
+		return size;    	
+	}
+	private boolean isEmpty() {
+		return size==0;
+	}
+	private boolean isFull() {
+		return size== queue.length;
+	}
+	public void enqueueFirst(T element) {
+		if(isFull()) {
+			throw new IllegalStateException("Queue is Full.");
+		}
+		if(isEmpty()) {
+			firstIndex = 0;
+			lastIndex = 0;
+		}
+		else {
+			firstIndex = (firstIndex-1 + queue.length) % queue.length;
+		}
+		queue[firstIndex] = element;
+		size++;
+	}
+
+	public void enqueueLast(T element) {
+		if (isFull()) {
+			throw new IllegalStateException("Queue is Full");			
+		}
+		if (isEmpty()) {
+			firstIndex= 0;
+			lastIndex = 0;
+		}
+		else {
+			lastIndex = (lastIndex+1)% queue.length;
+		}
+		queue[lastIndex] = element;
+		size++;
+	}
+
 }
