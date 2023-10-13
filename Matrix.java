2dac08dbc719a46112ed685dd9886836002384c6
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2023-10-13 12:37:35 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -2,40 +2,49 @@ import java.util.Random;
 
 public class MatrixOperations {
     public static void main(String[] args) {
-        int[][] matrix = new int[7][11];
+        int[][] matrix = new int[3][3];
         char[] operators = {'+', '-'};
 
-        // Initialize the matrix with random values and operations
+        // Initialize the 3x3 matrix with random values and operations
         Random rand = new Random();
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 11; j++) {
-                if (i % 2 == 0 && j % 2 == 0) {
-                    matrix[i][j] = rand.nextInt(9) + 1; // Random numbers between 1 and 9
-                } else if (i % 2 == 0 && j % 2 == 1) {
-                    matrix[i][j] = operators[rand.nextInt(2)]; // Random operator: + or -
-                } else {
-                    matrix[i][j] = '=';
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = rand.nextInt(9) + 1; // Random numbers between 1 and 9
+            }
+        }
+
+        // Generate random operations
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = matrix[i][j];
+                if (j < 2) {
+                    matrix[i][j + 1] = operators[rand.nextInt(2)]; // Random operator: + or -
                 }
             }
         }
 
         // Calculate the results
-        for (int i = 0; i < 7; i += 2) {
-            for (int j = 1; j < 10; j += 2) {
-                if (matrix[i][j] == '+') {
-                    matrix[i][j + 1] = matrix[i][j - 1] + matrix[i][j + 1];
-                } else {
-                    matrix[i][j + 1] = matrix[i][j - 1] - matrix[i][j + 1];
+        int r1 = matrix[0][0] + matrix[0][2];
+        int r2 = matrix[1][0] - matrix[1][2];
+        int r3 = matrix[2][0] + matrix[2][2];
+
+        // Print the 3x3 matrix with operations and '=' symbols
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j]);
+                if (j < 2) {
+                    System.out.print(" " + matrix[i][j + 1] + " ");
                 }
+            }
+            System.out.println();
+            if (i < 2) {
+                System.out.println("= = =");
             }
         }
 
-        // Print the matrix
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 11; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // Print the results
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
     }
 }
